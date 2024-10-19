5894aca2c179621659a72ef99931f3fdd1ec3b9e
Hernan Poncetta <hernanponcetta@gmail.com>
2024-10-19 19:03:28 +0200

    First commit.

---
+++ app/routes.ts
@@ -0,0 +1,3 @@
+import { index, RouteConfig } from "@react-router/dev/routes"
+
+export const routes: RouteConfig = [index("routes/home.tsx")]
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite"
+import { reactRouter } from "@react-router/dev/vite"
+import tsconfigPaths from "vite-tsconfig-paths"
+
+// @ts-expect-error process is a nodejs global
+const host = process.env.TAURI_DEV_HOST
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+  plugins: [reactRouter({ ssr: false }), tsconfigPaths()],
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+}))
